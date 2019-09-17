22124a3b4a5e1f43e76c9c1b3e893eeffad5efda
Shekhar Devnath <shekhar@solarreviews.com>
2019-09-17 22:24:02 +0930

    Route test

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,16 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+
+
+Route::get('/test', function () {
+    return view('welcome');
+});
