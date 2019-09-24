8e1f5a92f0387cb7c9334486fc9da155be3cac16
Shekhar Devnath <shekhar@solarreviews.com>
2019-09-24 14:03:12 +0930

    Allow admin to work with permissions

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,6 +55,9 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    @role('admin')
+                                        <a class="dropdown-item" href="{{ route('laravelroles::permissions.index') }}">Permissions</a>
+                                    @endrole
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
